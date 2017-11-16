427ad9839d4b7d7c58cd4176c2ce43bb816d705b
Geekparade <david_da_silva0hotmail.fr>
2017-11-16 16:18:20 +0100

    Exo1

--- JS-exercices/array/app.js
+++ JS-exercices/array/app.js
@@ -1,10 +1,10 @@
 var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
- console.log("coucou")
+ 
 
 
 //Exercice 1
 
 var Mois = ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'];
- console.log(Mois)
+ console.log(Mois[2])
 
 
---
+++ js/app.js
@@ -0,0 +1,8 @@
+var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+ 
+
+
+//Exercice 1
+
+var Mois = ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'];
+ console.log(Mois[2])
